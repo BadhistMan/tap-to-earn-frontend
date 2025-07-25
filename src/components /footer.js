c1200bd5bb577b763b5271bde425e95affec73a4
Aichat <badhistbest@gmail.com>
2025-07-26 00:00:12 +0300

    Create footer.js

---
+++ src/components /footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="mt-10 py-6 text-center text-gray-600">
+      <p>Created by Badhist Man 🇪🇹</p>
+      <p className="mt-2">
+        Join <a href="https://t.me/Freetech_1" className="text-blue-500 hover:underline">@Freetech_1</a> for updates
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
